c7a176ca516ebc95878f27e29a16db07f3443507
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2023-05-03 23:23:30 +0200

    feat/Added some functions to the registration form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,15 +4,26 @@ import { Link } from 'react-router-dom';
 import '../styles/registration.scss';
 
 const Register = () => {
+  const [tel, setTel] = useState('');
+  const [nip, setNip] = useState('');
   const [pwd, setPwd] = useState('');
   const [email, setEmail] = useState('');
   // const [role, setRole] = useState('');
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
 
   const handleChange = (e) => {
+    setTel(e.target.value);
+    setNip(e.target.value);
     setPwd(e.target.value);
     setEmail(e.target.value);
   };
 
+  console.log(tel);
+  console.log(nip);
   console.log(pwd);
   console.log(email);
 
@@ -31,25 +42,42 @@ const Register = () => {
           <option value='Pelnomocnik'>Pełnomocnik</option>
         </select>
         <label htmlFor='tel'>Phone</label>
-        <input className='registration__form-input' type='text' />
+        <input
+          className='registration__form-input'
+          placeholder='+48736768348'
+          type='text'
+          onChange={handleChange}
+        />
         <label htmlFor='nip'>NIP</label>
-        <input className='registration__form-input' type='text' />
+        <input
+          className='registration__form-input'
+          placeholder='1234567899'
+          type='text'
+          onChange={handleChange}
+        />
         <label htmlFor='email'>Email</label>
         <input
           className='registration__form-input'
-          type='text'
+          placeholder='youremail@gmail.com'
+          type='email'
           onChange={handleChange}
         />
         <label htmlFor='password'>Password</label>
         <input
           className='registration__form-input'
-          type='text'
-          id='message'
-          name='message'
+          type={passwordShown ? 'text' : 'password'}
+          id='password'
+          name='password'
           onChange={handleChange}
         />
+        <button onClick={togglePassword}>Show Password</button>
         <label htmlFor='confirmPassword'>Confirm Password </label>
-        <input className='registration__form-input' type='text' />
+        <input
+          className='registration__form-input'
+          type={passwordShown ? 'text' : 'password'}
+          id='password'
+          name='password'
+        />
       </form>
       <Link to='/login'>Sign In</Link>
     </section>
